694be750825f6de4168168337efed0a9f5a85d32
beanflame <68175497+beanflame@users.noreply.github.com>
2021-10-05 08:54:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,26 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python3
 
-VERSION = '1.0.4'
+from distutils.core import setup
 
-setup(
-    name="mkdocs-windmill",
-    version=VERSION,
-    url='https://github.com/gristlabs/mkdocs-windmill',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Documentation',
-        'Topic :: Text Processing',
-    ],
-    install_requires=[
-        'mkdocs',
-    ],
-    license='MIT',
-    description='MkDocs theme focused on navigation and usability',
-    author='Dmitry S',
-    author_email='dmitry@getgrist.com',
-    packages=find_packages(),
-    include_package_data=True,
-    entry_points={
-        'mkdocs.themes': [
-            'windmill = mkdocs_windmill',
-        ]
-    },
-    zip_safe=False
-)
+long_description = \
+"""This extension adds math formulas support to Python-Markdown_
+(works with version 2.6 or newer).
+
+.. _Python-Markdown: https://github.com/waylan/Python-Markdown
+
+You can find the source on GitHub_.
+Please refer to the `README file`_ for details on how to use it.
+
+.. _GitHub: https://github.com/mitya57/python-markdown-math
+.. _`README file`: https://github.com/mitya57/python-markdown-math/blob/master/README.md
+"""
+
+setup(name='python-markdown-math',
+      description='Math extension for Python-Markdown',
+      long_description=long_description,
+      author='Dmitry Shachnev',
+      author_email='mitya57@gmail.com',
+      version='0.2',
+      url='https://github.com/mitya57/python-markdown-math',
+      py_modules=['mdx_math'],
+      license='BSD')
